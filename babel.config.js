938167891dc1892e455e4e448daba27fb8368071
”surfwhz“ <surfwhz147258369@qq.com>
2021-07-21 18:33:02 +0800

    first commit

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-21 17:20:54
+ * @LastEditTime: 2021-07-21 17:50:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \cli2\supermall\babel.config.js
+ */
+module.exports = {
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
